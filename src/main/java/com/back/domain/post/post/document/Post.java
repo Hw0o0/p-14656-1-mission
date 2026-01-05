5f7d97652a89eb372751844c25bd223ac006b18a
leehyeonwoo <dlgusdn7706@gmail.com>
2026-01-05 17:35:36 +0900

    0019

--- src/main/java/com/back/domain/post/post/document/Post.java
+++ src/main/java/com/back/domain/post/post/document/Post.java
@@ -1,53 +1,31 @@
 package com.back.domain.post.post.document;
 
+import com.back.global.BaseDocument;
 import lombok.Data;
-import lombok.Getter;
-import org.springframework.data.annotation.*;
-import org.springframework.data.domain.Persistable;
-import org.springframework.data.elasticsearch.annotations.DateFormat;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
-import java.time.OffsetDateTime;
+
 
 @Document(indexName = "posts")
 @Data
-public class Post implements Persistable<String> {
-
-    @Id
-    private String id;
-    @Field(type = FieldType.Text)
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class Post extends BaseDocument<String> {
+    @Field(type= FieldType.Text)
     private String title;
-    @Field(type = FieldType.Text)
+    @Field(type= FieldType.Text)
     private String content;
-    @Field(type = FieldType.Keyword)
+    @Field(type= FieldType.Keyword)
     private String author;
 
-    @Field(
-        type = FieldType.Date,
-        format = DateFormat.date_time
-    )
-    @CreatedDate
-    private OffsetDateTime createdAt;
-
-    @Field(
-        type = FieldType.Date,
-        format = DateFormat.date_time
-    )
-    @LastModifiedDate
-    private OffsetDateTime lastModifiedAt;
-
-    public Post(String title, String content, String author) {
+    public Post(String title, String content, String author){
         this.title = title;
         this.content = content;
         this.author = author;
-        this.createdAt = OffsetDateTime.now();
-        this.lastModifiedAt = OffsetDateTime.now();
-    }
-
-    @Override
-    public boolean isNew() {
-        return id == null || (createdAt == null && lastModifiedAt == null);
     }
 }
