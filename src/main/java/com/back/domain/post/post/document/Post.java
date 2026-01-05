e2145e0089ceebba7c01038a60dce69b18e85afa
leehyeonwoo <dlgusdn7706@gmail.com>
2026-01-05 15:50:39 +0900

    0011

--- src/main/java/com/back/domain/post/post/document/Post.java
+++ src/main/java/com/back/domain/post/post/document/Post.java
@@ -1,11 +1,14 @@
+
+
 package com.back.domain.post.post.document;
 
-import java.time.OffsetDateTime;
-import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.*;
 import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.time.OffsetDateTime;
 
 @Document(indexName = "posts")
 public class Post {
@@ -29,4 +32,24 @@ public class Post {
         format = DateFormat.date_time
     )
     private OffsetDateTime lastModifiedAt;
+
+    public Post(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.createdAt = OffsetDateTime.now();
+        this.lastModifiedAt = OffsetDateTime.now();
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+            "id='" + id + '\'' +
+            ", title='" + title + '\'' +
+            ", content='" + content + '\'' +
+            ", author='" + author + '\'' +
+            ", createdAt=" + createdAt +
+            ", lastModifiedAt=" + lastModifiedAt +
+            '}';
+    }
 }
