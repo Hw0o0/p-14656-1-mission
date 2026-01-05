e3f490465f86ad5437ebe0396006afb70614f0db
leehyeonwoo <dlgusdn7706@gmail.com>
2026-01-05 17:26:12 +0900

    0018

--- src/main/java/com/back/domain/post/post/document/Post.java
+++ src/main/java/com/back/domain/post/post/document/Post.java
@@ -3,6 +3,7 @@ package com.back.domain.post.post.document;
 import lombok.Data;
 import lombok.Getter;
 import org.springframework.data.annotation.*;
+import org.springframework.data.domain.Persistable;
 import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
@@ -12,33 +13,41 @@ import java.time.OffsetDateTime;
 
 @Document(indexName = "posts")
 @Data
-public class Post {
+public class Post implements Persistable<String> {
+
     @Id
     private String id;
-    @Field(type= FieldType.Text)
+    @Field(type = FieldType.Text)
     private String title;
-    @Field(type= FieldType.Text)
+    @Field(type = FieldType.Text)
     private String content;
-    @Field(type= FieldType.Keyword)
+    @Field(type = FieldType.Keyword)
     private String author;
 
     @Field(
         type = FieldType.Date,
         format = DateFormat.date_time
     )
+    @CreatedDate
     private OffsetDateTime createdAt;
 
     @Field(
         type = FieldType.Date,
         format = DateFormat.date_time
     )
+    @LastModifiedDate
     private OffsetDateTime lastModifiedAt;
 
-    public Post(String title, String content, String author){
+    public Post(String title, String content, String author) {
         this.title = title;
         this.content = content;
         this.author = author;
         this.createdAt = OffsetDateTime.now();
         this.lastModifiedAt = OffsetDateTime.now();
+    }
+
+    @Override
+    public boolean isNew() {
+        return id == null || (createdAt == null && lastModifiedAt == null);
     }
 }
